dd49e8aa5d18bd15f5a638dd1efd828efd196910
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-09-14 14:18:04 +0300

    fix app file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import Statistics from '../components/Statistics/Statistics';
 import FriendList from '../components/FriendList/FriendList';
 import TransactionHistory from '../components/TransactionHistory/TransactionHistory';
 
-import user from '../components/data/user.json';
-import data from '../components/data/data.json';
-import friends from '../components/data/friends.json';
-import transactions from '../components/data/transactions.json';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
